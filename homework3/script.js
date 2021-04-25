99f1253b873b8270bac36cc4bdbddc38f4434c7e
Mykola Lukasevych <mukolka.lukas1912@gmail.com>
2021-04-25 19:34:34 +0300

    my third part to finish homework

---
+++ homework3/script.js
@@ -0,0 +1,11 @@
+function sum(){
+let a = Number(prompt("Enter first number"));
+let b = Number(prompt("Enter second number"));
+let c = a + b;
+if(a==b){
+    alert(3*c)
+}
+else{
+   alert(c); 
+}
+}
